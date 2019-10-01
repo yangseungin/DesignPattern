8c51731fa0049d783641d8756fcf63cf7e167791
seungin yang <rhfpdk92@naver.com>
2019-10-01 23:51:43 +0900

    파사드 패턴 구현하기

---
+++ src/main/java/com/giantdwarf/facade/case1/ComplexProduct.java
@@ -0,0 +1,11 @@
+package com.giantdwarf.facade.case1;
+
+public class ComplexProduct {
+    char[] nameChars = new char[10];
+
+    public ComplexProduct(){}
+
+    public void setFirstNameCharactor(char c){
+        nameChars[0]=c;
+    }
+}
